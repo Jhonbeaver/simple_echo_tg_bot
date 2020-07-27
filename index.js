9090909bf9ac63611f3541287654bfce2c492996
sidorovivan <ivan@geekdev.ee>
2020-07-27 20:42:10 +0500

    asd

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
-var io = require('socket.io')(5000);
+var cors = require('cors')
+const app = require('express')();
+const server = require('http').createServer(app);
+var io = require('socket.io')(server);
 io.set('origins', 'https://geekninja.ru');
 const { Telegraf } = require('telegraf')
+app.use(cors())
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
@@ -10,5 +14,9 @@ bot.on('message', (ctx) => {
         io.emit('chatmessage', `${ctx.message.from.username}: ${ctx.message.text}`);
     }
 })
+
+io.on('connection', socket => { /* ... */ });
+
+server.listen(3000)
 
 bot.launch()
